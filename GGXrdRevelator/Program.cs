6e93168c5ceae8a3385ffde96c50438ebc6f1bfc
Ethan Robards <yourtooslow@gmail.com>
2020-03-05 12:49:35 -0600

    Rework for AH3 + encryption + readme

--- GGXrdRevelator/Program.cs
+++ GGXrdRevelator/Program.cs
@@ -12,21 +12,30 @@ namespace GGXrdRevelator
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("GGXrdRevelator v0.1 by gdkchan");
+            Console.WriteLine("GGXrdRevelator v0.2 by gdkchan/Labryz");
             Console.ResetColor();
 
-            Console.WriteLine("Guilty Gear XRD -REVELATOR- UPK decrypter");
+            Console.WriteLine("Guilty Gear XRD -REVELATOR- UPK/Steam AH3 PAC decrypter");
 
             Console.WriteLine(string.Empty);
 
             uint OutVal = 0;
-
+            bool encrypt = false;
             if (args.Length > 0)
             {
-                switch (args[0].ToLower())
-                {
-                    case "-revel": OutVal = 0x72642a6f; break;
-                    case "-sign": OutVal = 0x43415046; break;
+                encrypt = args[0].Equals("-e");
+                if(!encrypt){
+                    switch (args[0].ToLower())
+                    {
+                        case "-revel": OutVal = 0x72642a6f; break;
+                        case "-sign": OutVal = 0x43415046; break;
+                    }
+                } else {
+                    switch (args[1].ToLower())
+                    {
+                        case "-revel": OutVal = 0x72642a6f; break;
+                        case "-sign": OutVal = 0x43415046; break;
+                    }
                 }
             }
 
@@ -36,27 +45,32 @@ namespace GGXrdRevelator
                 Console.WriteLine("Usage:");
                 Console.ResetColor();
 
-                Console.WriteLine("GGXrdRevelator (-revel|-sign) file.upk");
+                Console.WriteLine("GGXrdRevelator [-e] (-revel|-sign) infile");
                 Console.WriteLine("-revel  Decrypts GG Xrd REVELATOR files");
                 Console.WriteLine("-sign  Decrypts GG Xrd SIGN files");
+
+                Console.WriteLine("-e, when added, encrypts the input file. Otherwise, the program decrypts the file.");
 
                 Console.WriteLine(string.Empty);
 
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Example:");
+                Console.WriteLine("Examples:");
                 Console.ResetColor();
 
                 Console.WriteLine("GGXrdRevelator -revel ELP_VOICE_JPN_A_SF.upk");
+                Console.WriteLine("GGXrdRevelator -e -sign chara_split_28.pac");
 
                 Console.WriteLine(string.Empty);
 
-                Console.WriteLine("The output file name is the input name with .dec extension");
+                Console.WriteLine("The output file name is the input name with .dec or .enc extension\ndepending on if you used the -e flag.");
 
                 return;
             }
-
-            string FileName = args[1];
+            string FileName = encrypt ? args[2] : args[1];
             string Name = Path.GetFileName(FileName).ToUpper();
+            if (Name.EndsWith(".dec") || Name.EndsWith(".enc")){
+                Name = Name.Substring(0,Name.Length-4);
+            }
 
             uint Seed = 0;
 
@@ -70,18 +84,27 @@ namespace GGXrdRevelator
 
             using (FileStream Input = new FileStream(FileName, FileMode.Open))
             {
-                using (FileStream Output = new FileStream(FileName + ".dec", FileMode.Create))
+                string ext = encrypt ? ".enc" : ".dec"; 
+                using (FileStream Output = new FileStream(FileName + ext, FileMode.Create))
                 {
                     BinaryReader Reader = new BinaryReader(Input);
                     BinaryWriter Writer = new BinaryWriter(Output);
-
-                    while (Input.Position + 4 <= Input.Length)
-                    {
-                        Writer.Write(OutVal ^= Reader.ReadUInt32() ^ MT.GenRandomNumber());
-                    }
+                    if (!encrypt){
+                        while (Input.Position + 4 <= Input.Length)
+                        {
+                            Writer.Write(OutVal ^= Reader.ReadUInt32() ^ MT.GenRandomNumber());
+                        }
+                    } else {
+                        uint OldOut = OutVal;
+                        while(Input.Position + 4 <= Input.Length){
+                            uint NewOut = Reader.ReadUInt32();
+                            Writer.Write(OldOut ^ NewOut ^ MT.GenRandomNumber());
+                            OldOut = NewOut;
+                        
+                        }                       
+                    }      
                 }
             }
-
             Console.WriteLine("Finished!");
         }
     }
--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # GGXrdRevelator
 Guilty Gear Xrd -REVELATOR- and -SIGN- UPK decrypter.
 
-Read the usage to know how to use the tool.
+Also supports Arcana Heart 3: Love Max, LMSS, and LMSS Extend in SIGN mode.
 
-Special thanks to Altimor (which made the GG Xrd SIGN decryption tool).
+Usage:
+
+``GGXrdRevelatorDec [-e] (-sign|-revel) infile``
+
+-e, when specified, encerypts the input instead of decrypting it (useful for modding, optional)
+
+-sign works for Xrd -SIGN-, and any AH3 version including and after Love Max.  Specify -revel if you want to decrypt something from Revelator 1 or 2.
+
+You need either -sign or -revel to exist or else the program will fail.
+
+The output will be the input filename plus a ".dec" or ".enc" extension, depending on if you encrypted or decrypted.
+
+Special thanks to Altimor (which made the GG Xrd SIGN decryption tool) and gdkchan (who released this source + inadvertently solved AH3 encryption).
